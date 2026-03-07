1951618b84a9608df0c7efb99c59fd5447b7bad5
Melody Wang <113655447+Irisveil@users.noreply.github.com>
2026-03-07 13:35:52 -0700

    more views

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 We love Tomato Soup
 
 UI: https://www.figma.com/design/buGsOhi4ZlIvRWBQLE54IX/TomatoSoup?node-id=0-1&p=f&t=NeKRcQaKPUcDdmyL-0
+
+idea: friend system, adding new topics (admin only)
--- Soup/views.py
+++ Soup/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+def login_view(request):
+    """
+    Primary view to see all entries and their filtered variations 
+    """
+
+    return render(request, 'login.html')
+
+def signup_view(request):
+    """
+    Primary view to see all entries and their filtered variations 
+    """
+
+    return render(request, 'signup.html')
+
 # Create your views here.
+@login_required
 def home_view(request):
     """
     Primary view to see all entries and their filtered variations 
