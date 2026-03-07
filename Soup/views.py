c7491aac78d99ac7287350e5ab3a2211002917dd
Melody Wang <113655447+Irisveil@users.noreply.github.com>
2026-03-07 13:20:38 -0700

    Setting up URL and html

--- Soup/views.py
+++ Soup/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    """
+    Primary view to see all entries and their filtered variations 
+    """
+
+    # return render(request, 'home.html')
